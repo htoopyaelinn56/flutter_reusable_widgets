44eea56dd1e15b244a2bc408836b3c332a0ba58c
LynnKhantKyaw <lynnkhant.ky@gmail.com>
2023-05-17 21:23:02 +0630

    dispose latest time and date

--- example/lib/date_picker_example.dart
+++ example/lib/date_picker_example.dart
@@ -24,16 +24,76 @@ class _DateTimePickerExampleState extends State<DateTimePickerExample> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CommonOutlinedButton(
-              onSubmit: () async {
-                final date =
-                    await commonDatePicker(context: context, pickedDate: _date);
-                _date = date.toString().substring(0, 10);
-                setState(() {});
-              },
-              child: Text(
-                _date.isEmpty ? 'Date Picker' : _date,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CommonButton(
+                  onSubmit: () async {
+                    final date = await commonDatePicker(
+                        context: context, pickedDate: _date);
+                    _date = date.toString().substring(0, 10);
+                    setState(() {});
+                  },
+                  child: Text(
+                    _date.isEmpty ? 'Date Picker' : _date,
+                  ),
+                ),
+                20.hGap,
+                Row(
+                  children: [
+                    CommonOutlinedButton(
+                      onSubmit: () async {
+                        final date = await commonDatePicker(
+                            context: context, pickedDate: '');
+                        _date = date.toString().substring(0, 10);
+                        setState(() {});
+                      },
+                      child: Text(
+                        _date.isEmpty
+                            ? 'You don\'t want your lastest picked date'
+                            : _date,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            15.vGap,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CommonButton(
+                  onSubmit: () async {
+                    final time = await commonTimePicker(
+                        context: context,
+                        pickedTime: _time12HourFormat,
+                        use24HourFormat: false);
+                    _time12HourFormat = time!.format(context);
+                    setState(() {});
+                  },
+                  child: Text(
+                    _time12HourFormat.isEmpty
+                        ? 'Time Picker 12 hr format'
+                        : _time12HourFormat,
+                  ),
+                ),
+                15.hGap,
+                CommonOutlinedButton(
+                  onSubmit: () async {
+                    final time = await commonTimePicker(
+                        context: context,
+                        pickedTime: '',
+                        use24HourFormat: false);
+                    _time12HourFormat = time!.format(context);
+                    setState(() {});
+                  },
+                  child: Text(
+                    _time12HourFormat.isEmpty
+                        ? 'You don\'t want your latest picked time'
+                        : _time12HourFormat,
+                  ),
+                ),
+              ],
             ),
             15.vGap,
             CommonOutlinedButton(
@@ -47,22 +107,6 @@ class _DateTimePickerExampleState extends State<DateTimePickerExample> {
                 _time24HourFormat.isEmpty
                     ? 'Time Picker 24 hr format'
                     : _time24HourFormat,
-              ),
-            ),
-            15.vGap,
-            CommonOutlinedButton(
-              onSubmit: () async {
-                final time = await commonTimePicker(
-                    context: context,
-                    pickedTime: _time12HourFormat,
-                    use24HourFormat: false);
-                _time12HourFormat = time!.format(context);
-                setState(() {});
-              },
-              child: Text(
-                _time12HourFormat.isEmpty
-                    ? 'Time Picker 12 hr format'
-                    : _time12HourFormat,
               ),
             ),
           ],
